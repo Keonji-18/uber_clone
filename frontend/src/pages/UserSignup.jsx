056a887249f017a59b7896da3f70cf30cdeead61
Keonji-18 <suyash69pandey@gmail.com>
2026-02-28 10:24:12 +0530

    User Signup done

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const UserSignup = () => {
-    return ( 
-        <div>User Signup</div>
-     );
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [userdData, setUserData] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        setEmail('')
+        setPassword('')
+        setUserData({
+            userName: {
+                 firstName: firstName,
+                lastName : lastName },
+            email: email,
+            password: password,
+        })
+        
+
+    }
+    return (
+        <div className="p-7 h-screen flex flex-col justify-between">
+            <div>
+                <img className="w-16 mb-8" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="logo" />
+
+                <form onSubmit={(e) => {
+                    submitHandler(e)
+
+                    console.log(userdData);
+                }}>
+                    <h3 className="text-lg font-medium mt-4 mb-2">What's your name?</h3>
+                    <div className="mb-7 flex justify-around flex-row align-middle w-full gap-5">
+                        <input
+                            required
+                            type="text"
+                            placeholder="first name"
+                            className=" bg-[#eeee]  rounded px-4 py-3  w-full text-base placeholder:text-sm"
+                            value={firstName}
+                            onChange={(e)=>{
+                                setFirstName(e.target.value)
+                            }}
+                        />
+                        <input
+                            
+                            type="text"
+                            placeholder="last name"
+                            className=" bg-[#eeee] rounded px-4 py-3  w-full text-base placeholder:text-sm"
+                            value={lastName}
+                            onChange={(e)=>{
+                                setLastName(e.target.value)
+                            }}
+                        />
+                    </div>
+
+                    <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+                    <input
+                        required
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+
+                        }}
+                        className=" bg-[#eeee] mb-7 rounded px-4 py-3  w-full text-base placeholder:text-sm"
+                        type="email"
+                        placeholder="email@example.com" />
+
+                    <h3 className="text-lg font-medium mt-2 mb-2">Enter password</h3>
+                    <input
+                        required
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        className=" bg-[#eeee] mb-7  rounded px-4 py-3  w-full text-base  placeholder:text-sm"
+                        type="password"
+                        placeholder="password" />
+                    <button className="bg-black rounded  font-semibold px-4 py-2  text-white w-full text-lg mt-2 mb-3.5" >Sign-up User</button>
+                    <p className="text-center">Already have an account? <Link to="/login" className="text-blue-400">User login</Link></p>
+                </form>
+
+            </div>
+
+            <p className="text-[10px] text-gray-400 leading-tight">  By proceeding, you consent to get calls, WhatsApp or SMS
+                messages, including by automated means, from Uber and its affiliates to the number provided. </p>
+
+        </div>
+    );
 }
- 
+
 export default UserSignup;
